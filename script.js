c3e151c0f5a4da77d29646452abf585403b051c0
Baby Leezus <118462761+babyleezus@users.noreply.github.com>
2024-01-07 10:40:49 +0000

    added initial js elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const headsOne = document.getElementById('headsP1')
+const headsTwe = document.getElementById('headsP2')
+const tailsOne = document.getElementById('tailsP1')
+const tailsTwo = document.getElementById('tailsP1')
+const flipBtn = document.getElementById('flip-btn')
+const resetBtn = document.getElementById('reset-btn')
+
+headsOne.addEventListener('click', function(){
+    
+})
+
